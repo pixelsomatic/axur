5bb3caa12c18a764f3c5b5c8efd4771a0f57e160
krsnananda <krsnananda@outlook.com>
2020-02-01 13:03:10 -0300

    - Exibe lista de títulos

--- src/App.js
+++ src/App.js
@@ -1,41 +1,18 @@
 import React from 'react';
-import styled from "styled-components";
-import List from './components/List';
+import './App.css';
+import List from './pages/List';
 
 function App() {
   return (
-    <AppWrapper>
-      <AppHeader>
+    <div className="app-wrapper">
+      <header>
 
-      </AppHeader>
-      <body>
+      </header>
+      <div>
         <List/>
-      </body>
-    </AppWrapper>
+      </div>
+    </div>
   );
 }
 
 export default App;
-
-const AppWrapper = styled.div`
-  text-align: center;
-  :root{
-    --bg: #fdfdfd;
-    --highlight1: #ED4264;
-    --highlight2: #FFEDBC;
-    --color: #1a1e24;
-    --font-number: Montserrat, Roboto, Helvetica, Arial, sans-serif;
-    --font-head: "Space Mono", Consolas, Menlo, Monaco, "Courier New", monospace;
-    --font: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-  }
-`;
-
-const AppHeader = styled.header`
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`;
---
+++ src/services/api.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const api = axios.create({baseURL: 'http://www.mocky.io/v2/5be5e3fa2f000082000fc3f8'});
+
+export default api;
