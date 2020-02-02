d7b9501e4fb5251bd3a158fc13f136ced03cbc4a
krsnananda <krsnananda@outlook.com>
2020-02-02 02:08:58 -0300

    - Inicia sort de títulos com lodash

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import api from "../../services/api";
 import './styles.css';
+import {FormattedDate} from 'react-intl';
+import orderBy from 'lodash/orderBy';
 
 class List extends Component {
   state = {
-    books: []
+    books: [],
+    sortAscending: true
   };
   
   componentDidMount() {
@@ -14,30 +17,33 @@ class List extends Component {
   loadProducts = async () => {
     const response = await api.get();
     this.setState({ books: response.data });
+    
+    console.log(response.data);
   };
   
-  onClick(e) {
-    e.preventDefault();
-    this.setState({
-      books: this.state.books.reverse()
-    });
- }
-  
   render() {
+    const { books } = this.state;
+    const {teste} = orderBy(this.state.books, o => o.metadata.publishedAt)
     return (
       <div className="book-list">
-        {this.state.books.map((book, index) => (
-          <ol className="list">
+        {books.sort((a,b) => a.metadata.publishedAt - b.metadata.publishedAt)
+        .map((book, index) => (
+          <ol className="list" key={book.metadata.publishedAt}>
             <li className="item">
               <p className="number">{index +1}</p>
               <h2 className="headline" key={book.title}>{book.title}</h2>
+              <h4><FormattedDate
+                value={book.metadata.publishedAt}
+                day="numeric"
+                month="long"
+                year="numeric"
+                /></h4>
             </li>
           </ol>
         ))}
-        <button onClick={this.onClick.bind(this)}>Reverse</button>
+        <button onClick={teste}>teste</button>
       </div>
     );
   }
 }
 export default List;
-// this.state.fruits.map((fruit, index) => <li key={fruit} >{index + 1}. {fruit}</li>)
