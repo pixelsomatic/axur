3809fe7733608ad5c4552ef81ae7bf68800a4f47
krsnananda <krsnananda@outlook.com>
2020-02-05 22:16:19 -0300

    - Refatora espaçamento de código - Completa ordenação de posts de modo dinâmico

--- src/pages/List/index.js
+++ src/pages/List/index.js
@@ -2,53 +2,56 @@ import React, { Component } from "react";
 import api from "../../services/api";
 import "./styles.css";
 import { FormattedDate } from "react-intl";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default class List extends Component {
-  state = {
-    posts: []
-  };
+  	state = {
+	posts: []
+  	};
 
-  componentDidMount() {
-    this.loadProducts();
-  }
+  	componentDidMount() {
+		this.loadProducts();
+  	}
 
-  loadProducts = async () => {
-    const response = await api.get();
-    this.setState({ posts: response.data });
-  };
+  	loadProducts = async () => {
+		const response = await api.get();
+		this.setState({ posts: response.data });
+  	};
 
-  render() {
-    const { posts } = this.state;
-    return (
-      <div className="post-list">
-        <header className="contents">
-          <h1>Contents</h1>
-          <p>Latest Publications</p>
-        </header>
-        {posts
-          .sort((a, b) => b.metadata.publishedAt - a.metadata.publishedAt)
-          .map((post, index) => (
-            <ol className="list" key={post.metadata.publishedAt}>
-              <div className="card-w col-md-4">
-                <li className="item card neumorphism">
-                  <p> {index + 1} </p>
-                  <h2 key={post.title}>
-                    {post.title}
-                  </h2>
-                  <h4>
-                    <FormattedDate value={post.metadata.publishedAt}
-                      day="numeric" month="long" year="numeric"
-                    />
-                  </h4>
-                </li>
-              </div>
-            </ol>
-          ))}
-          <Link to="/publications">
-            <button> All Publications </button>
-          </Link>
-      </div>
-    );
-  }
+  	render() {
+		const { posts } = this.state;
+		
+		return (
+      	<div className="post-list">
+        		<header className="contents">
+					<h1>Contents</h1>
+          		<p>Latest Publications</p>
+        		</header>
+        		{posts
+					.sort((a, b) => b.metadata.publishedAt - a.metadata.publishedAt)
+					.map((post, index) => (
+            		<ol className="list" key={post.metadata.publishedAt}>
+              			<div className="card-w col-md-4">
+                			<li className="item card neumorphism">
+                  			<p> {index + 1} </p>
+                  			<h2 key={post.title}>{post.title}</h2>
+                  			<h4>
+										<FormattedDate
+											value={post.metadata.publishedAt}
+											day="numeric"
+											month="long"
+											year="numeric"
+										/>
+                  			</h4>
+                			</li>
+              			</div>
+            		</ol>
+          		))
+				}
+				<Link to="/publications">
+					<button> All Publications </button>
+				</Link>
+			</div>
+		);
+  	}
 }
--- src/pages/Publications/index.js
+++ src/pages/Publications/index.js
@@ -3,82 +3,91 @@ import api from "../../services/api";
 import authorApi from "../../services/authorApi";
 import "./styles.css";
 import { FormattedDate } from "react-intl";
-import {Link} from 'react-router-dom';
-import arrowBack from '../../assets/back.svg'
+import { Link } from "react-router-dom";
+import arrowBack from "../../assets/back.svg";
 export default class Publications extends Component {
- 
-    state = {
-    posts: [],
-    authors: []
-  };
+  	state = {
+   	posts: [],
+    	authors: []
+  	};
 
-	componentDidMount() {
-    this.loadProducts();
-  }
+  	componentDidMount() {
+   	this.loadProducts();
+  	}
 
-  loadProducts = async () => {
-    const response = await api.get();
-    this.setState({ posts: response.data });
-    
-    const responses = await authorApi.get();
-    this.setState({ authors: responses.data });
-  };
+  	loadProducts = async () => {
+   	const response = await api.get();
+    	this.setState({ posts: response.data });
 
-  sortAscending = (e) => {
-    const { posts } = this.state;
-    this.setState({ posts: posts })
-    posts.sort((a, b) => a.metadata.publishedAt - b.metadata.publishedAt)    
-    e.preventDefault();
-  }
+    	const responses = await authorApi.get();
+    	this.setState({ authors: responses.data });
+  	};
 
-  sortDescending = (e) => {
-    const { posts } = this.state;
-    this.setState({ posts: posts })
-    posts.sort((a, b) => b.metadata.publishedAt - a.metadata.publishedAt)
-    e.preventDefault();
-  }
+  	sortAscending = e => {
+   	const { posts } = this.state;
+   	this.setState({ posts: posts });
+   	posts.sort((a, b) => a.metadata.publishedAt - b.metadata.publishedAt);
+   	e.preventDefault();
+  	};
 
-  render() {
-    const { posts, authors } = this.state;
+  	sortDescending = e => {
+		const { posts } = this.state;
+		this.setState({ posts: posts });
+		posts.sort((a, b) => b.metadata.publishedAt - a.metadata.publishedAt);
+		e.preventDefault();
+  	};
 
-    return (
-      <div className="wrapper">
-        <div className="main-back">
-          <h1> All Publications </h1>
-          <Link to="/">
-            <button className="btn-back"> 
-              <img src={arrowBack} alt="arrow-back" /> 
-            </button>
-          </Link>
-        </div>
-        <div className="hover">
-          <span>Sort Posts</span>
-            <button className="sort-button" onClick={this.sortAscending}>Older Posts</button>
-            <button className="sort-button" onClick={this.sortDescending}>Recent Posts</button>
-        </div>
-      <div className="all-posts">
-        {authors.map((author) => 
-          <div className="card-details neumorphism" key={author.id}>
-            <span className="author-name"> Author: {author.name} </span>
-              <ul>
-                {posts.filter(p => p.metadata.authorId === author.id).map(post =>
-                  <li key={post.metadata.publishedAt}>
-                    <h2>{post.title}</h2>
-                    <span>{post.body}</span>
-                    <h5>
-                      Published at:   
-                      <FormattedDate 
-                        value={post.metadata.publishedAt}
-                        day="numeric" month="long" year="numeric"
-                      />
-                    </h5>
-                  </li>
-                )}
-              </ul>
-            </div>
-          )}
-      </div>
-    </div>
-    );
-  }
+  	getAuthorName(post) {
+		if (this.state.authors.length === 0) return "";
+		return this.state.authors.filter(a => a.id === post.metadata.authorId)[0]
+      .name;
+	}
+
+  	render() {
+		const { posts } = this.state;
+
+		return (
+			<div className="wrapper">
+				<div className="main-back">
+					<h1> All Publications </h1>
+					<Link to="/">
+						<button className="btn-back">
+							<img src={arrowBack} alt="arrow-back" />
+						</button>
+					</Link>
+				</div>
+				<div className="hover">
+					<span>Sort Posts</span>
+					<button className="sort-button" onClick={this.sortAscending}>
+						Older Posts
+					</button>
+					<button className="sort-button" onClick={this.sortDescending}>
+						Recent Posts
+					</button>
+				</div>
+				<div className="all-posts">
+					{posts.map(p => (
+						<div className="card-details neumorphism" key={p.title}>
+							<span className="author-name">
+								Author: {this.getAuthorName(p)}
+							</span>
+							<ul>
+								<h2> {p.title} </h2>
+								<span>{p.body}</span>
+								<h5>
+									Published at:
+									<FormattedDate
+										value={p.metadata.publishedAt}
+										day="numeric"
+										month="long"
+										year="numeric"
+									/>
+								</h5>
+							</ul>
+						</div>
+					))}
+				</div>
+			</div>
+		);
+  	}
 }
